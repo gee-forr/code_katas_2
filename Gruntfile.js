02f8d449e515c11d62db545269df5fcb4124f377
Gabriel Fortuna <gee.forr@gmail.com>
2014-02-22 14:37:18 +0200

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
+    jasmine : {
+      src:   'src/**/*.js',
+      specs: 'specs/**/*spec.js'
+    }
   });
 
+  // Register tasks.
+  grunt.loadNpmTasks('grunt-jasmine-runner');
 
+  // Default task.
+  grunt.registerTask('default', 'jasmine');
 };
